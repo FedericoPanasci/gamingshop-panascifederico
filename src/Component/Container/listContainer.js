216634f3955a6fe4e60556a0aff738df688aa213
federicopanasci <feedeeblk@gmail.com>
2022-05-09 19:17:51 -0300

    arreglada la list

--- src/Component/Container/listContainer.js
+++ src/Component/Container/listContainer.js
@@ -1,8 +1,6 @@
 const ListContainer = () => {
     return (
-        <li> item 1 </li>,
-        <li> item 2 </li>,
-        <li> item 3 </li>
+        <><li> item 1 </li><li> item 2 </li><li> item 3 </li></>
     )
 }
 export default ListContainer;
